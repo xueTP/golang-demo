ecdb9b490548a057e349f3ef665a21cddb4ba5bf
xuezd <xuezd@ename.com>
2018-11-20 18:53:23 +0800

    先提交

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"golang-demo/orm"
+	"fmt"
 	"github.com/Sirupsen/logrus"
 )
 
@@ -13,10 +14,14 @@ func main() {
 
 	class := orm.Class{Name: "t1 class"}
 	err := orm.DB.Create(&class).Error
-	user := orm.User{Name: "sum", ClassId: int32(class.ID)}
+	user := orm.User{Name: "sum", ClassId: int32(class.ClassId)}
 	err = orm.DB.Create(&user).Error
-	orm.DB.Model(&user).Related(&class)
 	if err != nil {
 		logrus.Errorf("err : %v", err)
 	}
+	otherUser := orm.User{}
+	orm.DB.Where("userName = ?", "sum").Find(&otherUser)
+	fmt.Printf("%+v", otherUser)
+	orm.DB.Model(&otherUser).Related(&otherUser.Class)
+	fmt.Printf("%+v", otherUser)
 }
--- orm/refer.go
+++ orm/refer.go
@@ -1,7 +1,8 @@
 package orm
 
 import (
-	"github.com/jinzhu/gorm"
+	"database/sql"
+	"time"
 )
 
 //select * from INFORMATION_SCHEMA.KEY_COLUMN_USAGE  where REFERENCED_TABLE_NAME='t_stu'
@@ -9,29 +10,43 @@ import (
 func CreateTable() {
 	if !DB.HasTable(&User{}) {
 		DB.Set("gorm:table_options", "ENGINE=Innodb").CreateTable(&User{})
-	}else {
+	} else {
 		DB.Set("gorm:table_options", "ENGINE=Innodb").AutoMigrate(&User{})
 	}
 	if !DB.HasTable(&Class{}) {
 		DB.Set("gorm:table_options", "ENGINE=Innodb").CreateTable(&Class{})
-	}else {
+	} else {
 		DB.Set("gorm:table_options", "ENGINE=Innodb").AutoMigrate(&Class{})
 	}
+	// 设置相应外键
+	DB.Model(&User{}).AddForeignKey("class_id", "class(`classId`)", "RESTRICT", "RESTRICT")
+}
+
+func DropTable() {
+	DB.DropTableIfExists(&User{})
+	DB.DropTableIfExists(&Class{})
 }
 
 type User struct {
-	UserId int32 `gorm:"primary_key"`
-	Name string `gorm:"column:userName;type: varchar(20);not null;"`
-	Age int8 `gorm:"default 0"`
-	Class Class // classId 为外键
+	UserId  int32  `gorm:"primary_key"`
+	Name    string `gorm:"column:userName;type: varchar(20);not null;"`
+	Age     int8   `gorm:"default 0"`
+	Class   Class  `gorm:"ForeignKey:ClassId;AssociationForeignKey:ClassId"` // classId 为外键
 	ClassId int32
 }
 
 type Class struct {
-	gorm.Model
-	Name string `gorm:"type: varchar(20);unique"`
+	ClassId int32  `gorm:"column:classId;primary_key;AUTO_INCREMENT"`
+	Name    string `gorm:"type: varchar(20);unique"`
+	Branch Branch
 }
 
 func (Class) TableName() string {
 	return "class"
+}
+
+type Branch struct {
+	BranchId   int32          `gorm:"column:branchId;primary_key;AUTO_INCREMENT;"`
+	BranchName sql.NullString `gorm:"column:branchName;type:varchar(40)"`
+	CreateTime time.Time      `gorm:"column:createTime"`
 }
